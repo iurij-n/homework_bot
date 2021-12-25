3aa4cde18271c357312555de1d9798c08869ebba
Iurij Novikov <iurij.novickov2016@yandex.ru>
2021-12-25 07:58:12 +0300

    222

--- 123.py
+++ 123.py
@@ -1,10 +1,23 @@
 from pprint import pprint
 import requests
+import time
+from telegram import Bot, ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
 TOKEN = 'AQAAAAASWs4aAAYckVnFE8x9YEaii-cSMWqvmBo'
+TELEGRAM_TOKEN = '5012792175:AAEeMJx4s2t3WQbMcfaX82DQ9qF8SzR23-4'
+TELEGRAM_CHAT_ID = 537063134
 url = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 headers = {'Authorization': f'OAuth {TOKEN}'}
-payload = {'from_date': 1639158976}
+payload = {'from_date': 1610103324}
+
+
+bot = Bot(token=TELEGRAM_TOKEN)
+updater = Updater(token=TELEGRAM_TOKEN)
+
+text = 'Вам телеграмма!'
+# Отправка сообщения
+bot.send_message(TELEGRAM_CHAT_ID, text)
 
 # Делаем GET-запрос к эндпоинту url с заголовком headers и параметрами params
 homework_statuses = requests.get(url, headers=headers, params=payload)
@@ -13,7 +26,16 @@ homework_statuses = requests.get(url, headers=headers, params=payload)
 #print(homework_statuses.text)
 
 # А можно ответ в формате JSON привести к типам данных Python и напечатать и его
+print('Текущее время - ', int(time.time()))
+type_answer = type(homework_statuses.json())
+print('Тип данных ответа API - ', type_answer)
+print(isinstance(type_answer, dict))
+if isinstance(homework_statuses.json(), dict):
+    print('Тип ответа словарь')
 pprint(homework_statuses.json())
+pprint(homework_statuses.json().get('homeworks'))
+
+
 # response = requests.get('https://www.swapi.tech/api/starships/9/')
 # pprint(response.json())
 #print(response.json().get('result').get('properties').get('name'))
--- homework.py
+++ homework.py
@@ -1,11 +1,14 @@
-...
+import requests
+import time
+from telegram import Bot, ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = 'AQAAAAASWs4aAAYckVnFE8x9YEaii-cSMWqvmBo'
+TELEGRAM_TOKEN = '5012792175:AAEeMJx4s2t3WQbMcfaX82DQ9qF8SzR23-4'
+TELEGRAM_CHAT_ID = 537063134
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -19,6 +22,10 @@ HOMEWORK_STATUSES = {
 }
 
 
+bot = Bot(token=TELEGRAM_TOKEN)
+updater = Updater(token=secret_token)
+
+
 def send_message(bot, message):
     ...
 
@@ -26,25 +33,19 @@ def send_message(bot, message):
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    homework = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    return homework.json()
 
 
 def check_response(response):
-
-    ...
+    if isinstance(response, dict):
+        return response.get('homeworks')
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
-
+    homework_name = homework.get('lesson_name')
+    homework_status = homework.get('status')
+    verdict = HOMEWORK_STATUSES.get(homework_status)
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
